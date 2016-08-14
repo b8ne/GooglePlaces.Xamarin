690e6eb605d05c53bc956b1be45db5ed8329010d
Ben <sutt8r@gmail.com>
2016-08-14 18:42:27 +1000

    Initial Commit

---
+++ Places/PlacesAutocomplete.cs
@@ -0,0 +1,25 @@
+﻿namespace GooglePlaces.Xamarin
+{
+	using System.Threading.Tasks;
+	using Newtonsoft.Json.Linq;
+
+	public class PlacesAutocomplete
+	{
+		private PlacesHttpProvider httpProvider;
+
+		public PlacesAutocomplete()
+		{
+			this.httpProvider = new PlacesHttpProvider();
+		}
+
+		public async Task<JObject> GetGoogleAutoComplete(string input)
+		{
+			string content = "input=" + input + "&components=country:au";
+			dynamic responseData = await httpProvider.FetchPostContentAsync(content).ConfigureAwait(false);
+			JObject predictions = responseData["predictions"];
+
+			return predictions;
+		}
+	}
+}
+
---
+++ Places/PlacesHttpProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GooglePlaces.Xamarin
+{
+	public class PlacesHttpProvider
+	{
+		private readonly HttpClient client;
+		private const string GooglePlacesAPI = "AIzaSyAgmGOT-0aURrQZ8vmvh1xNgRukpMhIcgU";
+		private const string GooglePlacesUrl = "https://maps.googleapis.com/maps/api/place/autocomplete/json";
+
+		public PlacesHttpProvider()
+		{
+			this.client = new HttpClient();
+		}
+
+		public async Task<JObject> FetchPostContentAsync(string postContent)
+		{
+			string uri = GooglePlacesUrl + "?" + postContent + "&key=" + GooglePlacesAPI;
+			var response = await this.client.PostAsync(new Uri(uri), new StringContent("")).ConfigureAwait(false);
+			var responseData = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+			response.EnsureSuccessStatusCode();
+
+			JObject returnData = JObject.Parse(responseData);
+
+			return returnData;
+		}
+	}
+}
+
